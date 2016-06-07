0415206f8371364df2b3698162238a83671e27e5
skjl <sudharsanakjl@rocketmail.com>
2016-06-07 21:30:06 +0530

    How to save the form and access the submitted details through admin page?

--- task2/task/views.py
+++ task2/task/views.py
@@ -1,29 +1,17 @@
-#from django.shortcuts import render
-#from django.http import HttpResponse
 
-# Create your views here.
-#def index(request):
-   #return HttpResponse("<h1>Hey there!</h1>")
-   #from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import NameForm
 
 def index(request):
-    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
         form = NameForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
+           
             NameForm.save()
             return HttpResponseRedirect('/thanks/')
 
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = NameForm()
 
